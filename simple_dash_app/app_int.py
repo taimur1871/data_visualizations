247249b4cbfdc3ab27481ca8be79ce322abdfbba
Rana Ahmad <taimur1871@gmail.com>
2021-05-08 22:10:19 -0600

    add map updating feature

---
+++ simple_dash_app/app_int.py
@@ -0,0 +1,114 @@
+import json
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
+df = pd.read_csv('/home/taimur/Documents/DarkCirrus Projects/Analyzing Bit Records/data_visualizations/simple_dash_app/data/Mid-Con ToolRun.csv')
+
+fig = px.scatter_mapbox(df, lat='Latitude', lon='Longitude', hover_data=['Official Well Name'],
+                    color_discrete_sequence=["red"],zoom=6)
+
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(clickmode='event+select')
+
+fig.update_traces(marker_size=6)
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='basic-interactions',
+        figure=fig
+    ),
+
+    html.Div(className='row', children=[
+        html.Div([
+            dcc.Markdown("""
+                **Hover Data**
+
+                Mouse over values in the graph.
+            """),
+            html.Pre(id='hover-data', style=styles['pre'])
+        ], className='three columns'),
+
+        html.Div([
+            dcc.Markdown("""
+                **Click Data**
+
+                Click on points in the graph.
+            """),
+            html.Pre(id='click-data', style=styles['pre']),
+        ], className='three columns'),
+
+        html.Div([
+            dcc.Markdown("""
+                **Selection Data**
+
+                Choose the lasso or rectangle tool in the graph's menu
+                bar and then select points in the graph.
+
+                Note that if `layout.clickmode = 'event+select'`, selection data also
+                accumulates (or un-accumulates) selected data if you hold down the shift
+                button while clicking.
+            """),
+            html.Pre(id='selected-data', style=styles['pre']),
+        ], className='three columns'),
+
+        html.Div([
+            dcc.Markdown("""
+                **Zoom and Relayout Data**
+
+                Click and drag on the graph to zoom or click on the zoom
+                buttons in the graph's menu bar.
+                Clicking on legend items will also fire
+                this event.
+            """),
+            html.Pre(id='relayout-data', style=styles['pre']),
+        ], className='three columns')
+    ])
+])
+
+
+@app.callback(
+    Output('hover-data', 'children'),
+    Input('basic-interactions', 'hoverData'))
+def display_hover_data(hoverData):
+    return json.dumps(hoverData, indent=2)
+
+
+@app.callback(
+    Output('click-data', 'children'),
+    Input('basic-interactions', 'clickData'))
+def display_click_data(clickData):
+    return json.dumps(clickData, indent=2)
+
+
+@app.callback(
+    Output('selected-data', 'children'),
+    Input('basic-interactions', 'selectedData'))
+def display_selected_data(selectedData):
+    return json.dumps(selectedData, indent=2)
+
+
+@app.callback(
+    Output('relayout-data', 'children'),
+    Input('basic-interactions', 'relayoutData'))
+def display_relayout_data(relayoutData):
+    return json.dumps(relayoutData, indent=2)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- simple_dash_app/main.py
+++ simple_dash_app/main.py
@@ -10,12 +10,34 @@ import pandas as pd
 
 df = pd.read_csv('./data/Mid-Con ToolRun.csv')
 
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(
     [
+        html.Center(
+            html.H3('Bit Offset Analysis')
+            ),
+        html.Div(
+            [
+                dcc.RadioItems(
+                    id='application',
+                    options=[{'label':i, 'value':i} for i in df['Dir Type'].unique()],
+                    value = 'V',
+                    labelStyle={'display':'inline-block'}
+                ),
+                dcc.Dropdown(
+                    id='bit-size-dropdown',
+                    options = [{'label':i, 'value':i} for i in df['Bit Size'].unique()],
+                    value=12.25,
+                ),
+                dcc.Graph(id='map-w-radio'),
+            ],
+            className="pretty_container six columns",
+            id="map"
+        ),
         html.Div(
             [
                 dcc.Dropdown(
@@ -30,16 +52,29 @@ app.layout = html.Div(
         ),
         html.Div(
             [
-                dcc.Graph(id='map-w-radio'),
-                dcc.RadioItems(
-                    id='application',
-                    options=[{'label':i, 'value':i} for i in df['Dir Type'].unique()],
-                    value = 'H',
-                    labelStyle={'display':'inline-block'}
-                )
+                dcc.Dropdown(
+                    id='fm',
+                    options = [{'label':i, 'value':i} for i in df['Formation Summary'].unique()],
+                    value='MRMC',
+                ),
+                dcc.Graph(id='scatter-w-radio'),
             ],
             className="pretty_container six columns",
-            id="map"
+            id="scatter"
+        ),
+        html.Div(
+            [
+                dcc.Slider(
+                    id='distance',
+                    min=df['Distance'].min(),
+                    max=df['Distance'].max(),
+                    value=df['Distance'].min(),
+                    step=None,
+                ),
+                dcc.Graph(id='scatter-perf'),
+            ],
+            className="pretty_container five columns",
+            id="scatter-int"
         ),
     ]
 )
@@ -47,12 +82,12 @@ app.layout = html.Div(
 
 @app.callback(
     Output('graph-with-slider', 'figure'),
-    Input('year-slider', 'value'))
-def update_figure(selected_year):
-    filtered_df = df[df['Bit Size'] == selected_year]
+    Input('year-slider', 'value'),
+    Input('application', 'value'))
+def update_figure(bit_size, dir_type):
+    filtered_df = df[(df['Bit Size'] == bit_size) & (df['Dir Type']==dir_type)]
 
-    fig = px.scatter(filtered_df, x="Distance", y="ROP", color="Bit Mfg",
-                     log_x=False, size_max=55)
+    fig = px.scatter(filtered_df, x="Distance", y="ROP", color="Bit Mfg")
 
     fig.update_layout(transition_duration=0)
 
@@ -60,14 +95,40 @@ def update_figure(selected_year):
 
 @app.callback(
     Output('map-w-radio', 'figure'),
-    Input('application', 'value'))
-def update_figure(application):
-    filtered_df = df[df['Dir Type'] == application]
+    Input('application', 'value'),
+    Input('bit-size-dropdown','value'))
+def update_figure(application, bit_size):
+    filtered_df = df[(df['Dir Type'] == application) & (df['Bit Size']==bit_size)]
 
     fig = px.scatter_mapbox(filtered_df, lat='Latitude', lon='Longitude',
                             color_discrete_sequence=["red"],zoom=6)
 
     fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(transition_duration=0)
+
+    return fig
+
+@app.callback(
+    Output('scatter-w-radio', 'figure'),
+    Input('fm','value'))
+def update_figure(fm):
+    filtered_df = df[df['Formation Summary']==fm]
+
+    fig = px.scatter(filtered_df, x="Distance", y="ROP", color="Bit Mfg")
+
+    fig.update_layout(transition_duration=0)
+
+    return fig
+
+@app.callback(
+    Output('scatter-perf', 'figure'),
+    Input('distance', 'value'),
+    )
+def update_figure(dist):
+    filtered_df = df[df['Distance'] > int(dist)]
+
+    fig = px.scatter(filtered_df, x="Distance", y="ROP", color="Bit Mfg")
+
     fig.update_layout(transition_duration=0)
 
     return fig
